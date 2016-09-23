f2ec82149b4f9b52f76e1e8bc33245aeb7506447
Paul <pacalabre@gmail.com>
2016-09-23 11:21:17 -0700

    added the rest of the tutorial code

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-
+// init ScrollMagic Controller
 var controller = new ScrollMagic.Controller();
 
-var scene = new ScrollMagic.Scene({
-  triggerElement: '#pinned-trigger1', // starting scene, when reaching this element
-  duration: 400 // pin the element for a total of 400px
+// Scene Handler
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#pinned-trigger1", // point of execution
+  duration: $(window).height() - 20, // pin element for the window height - 1
+  triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
+  reverse: true // allows the effect to trigger when scrolled in the reverse direction
 })
-.setPin('#pinned-element1'); // the element we want to pin
+.setPin("#pinned-element1") // the element we want to pin
+.addTo(controller);
 
-// Add Scene to ScrollMagic Controller
-controller.addScene(scene);
-
-// Add Scene to ScrollMagic Controller
-controller.addScene([
-  scene1,
-  scene2,
-  scene3
-]);
+// Scene2 Handler
+var scene2 = new ScrollMagic.Scene({
+  triggerElement: "#pinned-trigger2", // point of execution
+  duration: 500 // pin the element for a total of 400px
+})
+.setPin("#pinned-element2") // the element we want to pin
+.addTo(controller);
